8cb4e813c8b865639c4a76d75cdc5048ad43dd7f
Bryan Escobar <50363542+Beck2301@users.noreply.github.com>
2021-07-01 15:08:18 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
-$(document).ready(function()
-{
-$("#calcular").click(function () {
 
-  var nro1 = parseFloat( $("#dato").val() );
-  var nro2 = parseFloat( $("#dato2").val() );
-  var suma = nro1 * nro2;     
-  $("#resultado").val(suma);
 
-  var totalcuenta= suma + nro1;
+$(document).ready(function () {
 
-  $("#totalcuenta").val(totalcuenta);
+ 
+
+
+  $(document).on('input', function () {
+
+    var nro1 = parseFloat($("#dato").val());
+    var nro2 = parseFloat($("#dato2").val());
+
+    var suma = nro1 * nro2;
+    $("#resultado").val(suma);
+    var totalcuenta = suma + nro1;
+    $("#totalcuenta").val(totalcuenta);
+
+
+  });
+
+
+
+
 
 });
-});
+
